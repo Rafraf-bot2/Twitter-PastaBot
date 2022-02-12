7840b528c00b98044ce7bf260ffbf2dd104e1daa
Rafraf-bot2 <soulstories360@gmail.com>
2022-02-12 14:26:41 +0100

    Generation of random tweet from bd.json added | gitignore modified

--- Bot/pastaBot.py
+++ Bot/pastaBot.py
@@ -1,6 +1,19 @@
 import tweepy
 from config import create_client
+import json
+import random
 
-client = create_client()
+def randTroll():
+    with open('bd.json', encoding='utf-8') as jsonFile:
+        jsonObj = json.load(jsonFile)
+        jsonFile.close
+    numMsgRand = random.randint(0, len(jsonObj['tweet'])-1)
+    return jsonObj['tweet'][numMsgRand]['message']
+    
+def main():
+    client = create_client()
+    troll = randTroll()
+    print(troll)
 
-response = client.create_tweet(text='hello world')
+if __name__ == "__main__":
+    main()
